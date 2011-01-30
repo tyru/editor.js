e1987423c37d31705b4b45843c1f096f2509d008
tyru <tyru.exe@gmail.com>
2011-01-30 15:07:03 +0900

    create keyTable

--- editor.js
+++ editor.js
@@ -1,13 +1,68 @@
 (function () {
-    function getKeyChar(e) {
-        var keyChar = String.fromCharCode(e.keyCode);
-        keyChar = e.shiftKey ? keyChar.toUpperCase() : keyChar.toLowerCase();
+    var Commands = {
+        moveDown: function (info) {
+            console.log("down");
+        },
+        moveUp: function (info) {
+            console.log("up");
+        },
+        moveLeft: function (info) {
+            console.log("left");
+        },
+        moveRight: function (info) {
+            console.log("right");
+        },
+    };
+
+    var Editor = (function () {
+        var NORMAL_MODE = 0,
+            INSERT_MODE = 1;
+
+        var Editor = {
+            mode: NORMAL_MODE,
+            dispatchKey: function (char, event) {
+                var info = {
+                    'char': char,
+                    'event': event,
+                    'mode': this.mode,
+                };
+                if (this.keyTable[this.mode][char])
+                    this.keyTable[this.mode][char](info);
+                else
+                    this.defaultKeyTable[this.mode](info);
+            },
+            keyTable: {},
+            defaultKeyTable: {},
+        };
+        Editor.keyTable[NORMAL_MODE] = {
+            'j': Commands.moveDown,
+            'k': Commands.moveUp,
+            'h': Commands.moveLeft,
+            'l': Commands.moveRight,
+        };
+        Editor.defaultKeyTable[NORMAL_MODE] =
+            function (info) { /* ignore */ };
+
+        return Editor;
+    })();
+
+
+    function getKeyChar(event) {
+        if (!event.keyCode)
+            return null;
+        var keyChar = String.fromCharCode(event.keyCode);
+        keyChar = event.shiftKey ? keyChar.toUpperCase() : keyChar.toLowerCase();
         return keyChar;
     }
-    function dispatchKey(e) {
-        e = e || window.event;
-        var char = getKeyChar(e);
-        console.log("input: " + char);
+    function dispatchKey(event) {
+        event = event || window.event;
+        var char = getKeyChar(event);
+        if (typeof(char) !== typeof("")) {
+            console.log("error: invalid key char.");
+            return;
+        }
+        console.log("input: " + uneval(char));
+        Editor.dispatchKey(char, event);
     }
     function onLoad() {
         if (!Prototype.Browser.Gecko) {
